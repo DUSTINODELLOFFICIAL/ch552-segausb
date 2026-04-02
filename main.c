664b256dae862711721441ff4cf29fe4126af87f
32mbit <dustinodell0@gmail.com>
2026-04-02 07:44:10 -0500

    refactor a bit

--- main.c
+++ main.c
@@ -59,77 +59,98 @@ const uint16_t alt_map[] = {
   0x0200
 };
 
+uint16_t pad;
 uint16_t* btn_map;
 
 void USB_interrupt(void);
+static inline void select_high(void);
+static inline void select_low(void);
+void poll_pad(void);
+void make_report(void);
+
 void USB_ISR(void) __interrupt(INT_NO_USB) {
   USB_interrupt();
 }
 
 #pragma disable_warning 110
 
-__xdata uint8_t pad1_report[]   = {0,0,0,0,0};
+static inline void select_high() {
+  PIN_high(PIN_SELECT);
+  DLY_us(2);
+}
+
+static inline void select_low() {
+  PIN_low(PIN_SELECT);
+  DLY_us(2);
+}
+
+__xdata uint8_t pad1_report[] = {0,0,0,0,0};
 
 void poll_pad() {
-  uint16_t pad = 0;
   bool is_6btn = false;
 
+  pad = 0;
+
+  for(uint8_t counter = 0; counter < 8; counter++) {
+    switch(counter) {
+      case 0:
+        select_low();
+        pad |= (PIN_read(PIN_AB) ? 0 : btn_map[CTRL_A]) |
+               (PIN_read(PIN_SC) ? 0 : btn_map[CTRL_START]);
+        break;
+      case 1:
+        select_high();
+        pad |= (PIN_read(PIN_UP)    ? 0 : btn_map[CTRL_UP])    |
+               (PIN_read(PIN_DOWN)  ? 0 : btn_map[CTRL_DOWN])  |
+               (PIN_read(PIN_LEFT)  ? 0 : btn_map[CTRL_LEFT])  |
+               (PIN_read(PIN_RIGHT) ? 0 : btn_map[CTRL_RIGHT]) |
+               (PIN_read(PIN_AB)    ? 0 : btn_map[CTRL_B])     |
+               (PIN_read(PIN_SC)    ? 0 : btn_map[CTRL_C]);
+        break;
+      case 2:
+        select_low();
+        break;
+      case 3:
+        select_high();
+        break;
+      case 4:
+        select_low();
+        if (!PIN_read(PIN_UP) && !PIN_read(PIN_DOWN)) {
+          is_6btn = true;
+        }
+        break;
+      case 5:
+        select_high();
+        if (is_6btn) {
+          pad |= (PIN_read(PIN_UP)    ? 0 : btn_map[CTRL_Z]) |
+                 (PIN_read(PIN_DOWN)  ? 0 : btn_map[CTRL_Y]) |
+                 (PIN_read(PIN_LEFT)  ? 0 : btn_map[CTRL_X]) |
+                 (PIN_read(PIN_RIGHT) ? 0 : btn_map[CTRL_MODE]);
+        }
+        break;
+      case 6:
+        select_low();
+        break;
+      case 7:
+        select_high();
+        break;
+    }
+  }
+}
+
+void make_report() {
   pad1_report[0] = 1; // pad ID
-  pad1_report[1] = 128;
-  pad1_report[2] = 128;
-  pad1_report[3] = 0;
-  pad1_report[4] = 0;
-
-  PIN_low(PIN_SELECT);
-  DLY_us(5);
-  pad |= (PIN_read(PIN_AB) ? 0 : btn_map[CTRL_A]) |
-        (PIN_read(PIN_SC) ? 0 : btn_map[CTRL_START]);
-
-  PIN_high(PIN_SELECT);
-  DLY_us(5);
-  pad |= (PIN_read(PIN_UP)    ? 0 : btn_map[CTRL_UP])    |
-        (PIN_read(PIN_DOWN)  ? 0 : btn_map[CTRL_DOWN])  |
-        (PIN_read(PIN_LEFT)  ? 0 : btn_map[CTRL_LEFT])  |
-        (PIN_read(PIN_RIGHT) ? 0 : btn_map[CTRL_RIGHT]) |
-        (PIN_read(PIN_AB)    ? 0 : btn_map[CTRL_B])     |
-        (PIN_read(PIN_SC)    ? 0 : btn_map[CTRL_C]);
-
-  PIN_low(PIN_SELECT);
-  DLY_us(5);
-
-  PIN_high(PIN_SELECT);
-  DLY_us(5);
-
-  PIN_low(PIN_SELECT);
-  DLY_us(5);
-  if (!PIN_read(PIN_UP) && !PIN_read(PIN_DOWN)) {
-    is_6btn = true;
-  }
-
-  PIN_high(PIN_SELECT);
-  DLY_us(5);
-  if (is_6btn) {
-    pad |=  (PIN_read(PIN_UP)    ? 0 : btn_map[CTRL_Z]) |
-          (PIN_read(PIN_DOWN)  ? 0 : btn_map[CTRL_Y]) |
-          (PIN_read(PIN_LEFT)  ? 0 : btn_map[CTRL_X]) |
-          (PIN_read(PIN_RIGHT) ? 0 : btn_map[CTRL_MODE]);
-  }
-
-  PIN_low(PIN_SELECT);
-  DLY_us(5);
-  PIN_high(PIN_SELECT);
-  DLY_us(5);
 
   if (pad & btn_map[CTRL_LEFT]) pad1_report[1] = 0;
   else if (pad & btn_map[CTRL_RIGHT]) pad1_report[1] = 255;
+  else pad1_report[1] = 128;
 
   if (pad & btn_map[CTRL_UP]) pad1_report[2] = 0;
   else if (pad & btn_map[CTRL_DOWN]) pad1_report[2] = 255;
+  else pad1_report[2] = 128;
 
   pad1_report[3] = (uint8_t) (pad & 0xFF);
   pad1_report[4] = (uint8_t) ((pad >> 8) & 0x03); // Buttons 9 & 10 (start & mode)
-
-  HID_sendReport(pad1_report, sizeof(pad1_report));
 }
 
 void main(void) {
@@ -146,7 +167,9 @@ void main(void) {
   HID_init();
 
   while (1) {
-    DLY_ms(2);
     poll_pad();
+    make_report();
+    HID_sendReport(pad1_report, sizeof(pad1_report));
+    DLY_ms(2);
   }
 }
